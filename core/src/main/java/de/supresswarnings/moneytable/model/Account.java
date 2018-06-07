dc0c7dd3070b863df083dd9c4729b5894014e940
Constantin Schulte <constantinschulte@web.de>
2018-06-07 11:32:41 +0200

    Add account and account test.

--- core/src/main/java/de/supresswarnings/moneytable/model/Account.java
+++ core/src/main/java/de/supresswarnings/moneytable/model/Account.java
@@ -1,4 +1,53 @@
 package de.supresswarnings.moneytable.model;
 
+import de.supresswarnings.moneytable.model.transaction.Transaction;
+import de.supresswarnings.moneytable.model.transaction.TransactionGroup;
+
+import java.util.ArrayList;
+
 public class Account {
+
+    private String name;
+    private double current;
+    private ArrayList<TransactionGroup> transactionGroups;
+    private ArrayList<Transaction> transactions;
+
+    public Account(String name, double current){
+        this.name = name;
+        this.current = current;
+        transactionGroups = new ArrayList<>();
+        transactions = new ArrayList<>();
+    }
+
+    public void add(Transaction transaction){
+        transactions.add(transaction);
+    }
+
+    public void add(TransactionGroup group){
+        transactionGroups.add(group);
+    }
+
+    public void setCurrent(double current) {
+        this.current = current;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCurrent() {
+        return current;
+    }
+
+    public ArrayList<TransactionGroup> getTransactionGroups() {
+        return transactionGroups;
+    }
+
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
 }
--- core/src/test/java/de/supresswarnings/moneytable/model/AccountTest.java
+++ core/src/test/java/de/supresswarnings/moneytable/model/AccountTest.java
@@ -1,4 +1,40 @@
 package de.supresswarnings.moneytable.model;
 
+import de.supresswarnings.moneytable.model.transaction.TransactionGroup;
+import de.supresswarnings.moneytable.model.transaction.UniqueTransaction;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class AccountTest {
+
+    @Test
+    public void test(){
+        String name = "test";
+        double current = 1.23;
+
+        Account account = new Account(name, current);
+
+        Assert.assertEquals(name, account.getName());
+        Assert.assertEquals(current, account.getCurrent(), 0.0);
+
+        UniqueTransaction transaction = new UniqueTransaction("test", System.currentTimeMillis(), 1.23);
+        account.add(transaction);
+
+        Assert.assertEquals(transaction, account.getTransactions().get(0));
+
+        current = 2.34;
+        account.setCurrent(current);
+
+        Assert.assertEquals(current, account.getCurrent(), 0.0);
+
+        name = "Name";
+        account.setName(name);
+
+        Assert.assertEquals(name, account.getName());
+
+        TransactionGroup group = new TransactionGroup("test");
+        account.add(group);
+
+        Assert.assertEquals(group, account.getTransactionGroups().get(0));
+    }
 }
