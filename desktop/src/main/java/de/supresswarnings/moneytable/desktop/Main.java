f0464a76f9b43ec39ff58834e81c70a78d4c20aa
Constantin Schulte <constantinschulte@web.de>
2018-12-08 01:35:21 +0100

    Remove System.out.println

--- desktop/src/main/java/de/supresswarnings/moneytable/desktop/Main.java
+++ desktop/src/main/java/de/supresswarnings/moneytable/desktop/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static final Logger LOGGER = new Logger();
 
     public static void main(String[] args){
-        System.out.println("Welcome to MoneyTable");
+        LOGGER.log("Welcome to MoneyTable");
+        LOGGER.writeLog();
     }
 }
