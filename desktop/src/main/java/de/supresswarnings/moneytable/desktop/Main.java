8d7386c123e5014165ebc959d1af4c1649731bdc
Constantin Schulte <constantinschulte@web.de>
2018-12-11 23:17:31 +0100

    Add documentation and improve Logger

--- desktop/src/main/java/de/supresswarnings/moneytable/desktop/Main.java
+++ desktop/src/main/java/de/supresswarnings/moneytable/desktop/Main.java
@@ -2,9 +2,23 @@ package de.supresswarnings.moneytable.desktop;
 
 import de.supresswarnings.moneytable.desktop.settings.Logger;
 
+/**
+ * Contains the start method for the application.
+ *
+ * @author Constantin Schulte
+ * @version 0.1-20181211
+ */
 public class Main {
+    /**
+     * The project-level logger object.
+     */
     public static final Logger LOGGER = new Logger();
 
+    /**
+     * Starts the application.
+     *
+     * @param args (irrelevant so far)
+     */
     public static void main(String[] args){
         LOGGER.log("Welcome to MoneyTable");
         LOGGER.writeLog();
--- desktop/src/main/java/de/supresswarnings/moneytable/desktop/settings/Logger.java
+++ desktop/src/main/java/de/supresswarnings/moneytable/desktop/settings/Logger.java
@@ -2,19 +2,43 @@ package de.supresswarnings.moneytable.desktop.settings;
 
 import java.io.*;
 
+/**
+ * This custom-made Logger creates a text file (.txt). It helps understanding errors in production versions.
+ *
+ * @author Constantin Schulte
+ */
 public class Logger {
 
+    /**
+     * The object representation of the log file.
+     */
     private File logFile;
+
+    /**
+     * The (new) content of the log file.
+     */
     private String log;
+
+    /**
+     * To create time stamps, the start time of the application is important.
+     */
     private long startTime = System.currentTimeMillis();
 
+    /**
+     * Initializes the log-String and the logFile-File.
+     * This includes creating a new file on the hard-drive if it does not exist yet.
+     */
     public Logger(){
         log = "";
         logFile = new File(System.getProperty("user.home") + "/.moneytable/logging/log.txt");
         if(!logFile.exists()){
-            logFile.getParentFile().mkdirs();
+            if(!logFile.getParentFile().mkdirs()){
+                System.err.println("Failed to create directory for log file.");
+            }
             try {
-                logFile.createNewFile();
+                if(!logFile.createNewFile()){
+                    System.err.println("Failed to create log file.");
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -28,6 +52,11 @@ public class Logger {
         log("INFO: Logging started");
     }
 
+    /**
+     * Adds a specific event description to the log-String, together with its timestamp.
+     *
+     * @param logEntry the description of the event
+     */
     public void log(String logEntry){
         long logTime = System.currentTimeMillis() - startTime;
         String timePart = "[" + (logTime / (1000*60*60)) + ":";
@@ -40,14 +69,24 @@ public class Logger {
         log += timePart + logEntry + "\n";
     }
 
+    /**
+     * Logs an exception as a description and the actual error message.
+     *
+     * @param logEntry the message about the error, especially containing an error code to identify its root
+     * @param e the actual exception, which message will be logged too
+     */
     public void logException(String logEntry, Exception e){
         log(logEntry);
         log(e.getMessage());
         writeLog();
     }
 
+    /**
+     * Appends the log-String to the logFile-File.
+     * Necessary for the log to be permanently stored.
+     */
     public void writeLog() {
-        try(PrintWriter stream = new PrintWriter(new FileOutputStream(logFile, true));) {
+        try(PrintWriter stream = new PrintWriter(new FileOutputStream(logFile, true))) {
             stream.append(log);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
